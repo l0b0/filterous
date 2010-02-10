908af833419cb7d8939937976d5f21574404b9b7
Victor Engmark <victor.engmark@gmail.com>
2010-02-10 16:25:23 +0000

    Works with simple tests of AND and NOT operators for tags.
    git-svn-id: https://filterous.svn.sourceforge.net/svnroot/filterous@3 5632bb97-7f59-4fb5-8ea0-cd9efd6acbbd

--- filterous.py
+++ filterous.py
@@ -7,14 +7,14 @@ Default syntax:
 ./filterous.py < all.xml
 
 Options:
---tag       Tag search string (@tag)
---ntag      Negated tag search string (@tag)
---desc      Description search string (@description)
---ndesc     Negated description search string (@description)
---notes     Notes search string (@extended)
---nnotes    Negated notes search string (@extended)
---url       URL search string (@href)
---nurl      Negated URL search string (@href)
+--tag      Tag search string (@tag)
+--ntag     Negated tag search string (@tag)
+--desc     Description search string (@description)
+--ndesc    Negated description search string (@description)
+--note     Note search string (@extended)
+--nnote    Negated note search string (@extended)
+--url      URL search string (@href)
+--nurl     Negated URL search string (@href)
 
 Description:
 Will search the tags of the XML file downloaded from
@@ -31,10 +31,10 @@ Examples:
 ./filterous.py --tag=video --tag=tosee --ntag=seen < all.xml
     Returns links to unseen videos.
 
-./filterous.py --nurl='^https' < all.xml
+./filterous.py --nurl=^https < all.xml
     Returns non-HTTPS links.
 
-./filterous.py --ntag='^for:' --tag='★★★★★' < all.xml
+./filterous.py --ntag=^for: --tag=★★★★★ < all.xml
     Returns great links that you have not shared with your contacts.
 """
 
@@ -44,36 +44,88 @@ __url__ = 'http://filterous.sourceforge.net/'
 __copyright__ = 'Copyright (C) 2010 Victor Engmark'
 __license__ = 'GPLv3'
 
-from datetime import datetime
 import xml.etree.ElementTree as ET
-import re
 
-class Bookmarks():
+TAG_SEPARATOR = u' '
+
+class DeliciousBookmarks(ET.ElementTree):
     """Has a list of bookmarks with their metadata and display functions."""
-    def __init__(self, text):
-        self.doc = ET.fromstring(text)
 
-    def __str__(self):
-        return ET.tostring(self.doc)
+    def search_tags_and(self, tags):
+        """
+        Check that all tags are present. Remove the bookmark if not.
 
-    def search(self, tags):
+        @param tags: List of tags
+        """
+        root = self.getroot()
+        bookmarks = root.getchildren()
+        garbage = []
+        for tag in tags:
+            for bookmark in bookmarks:
+                if tag not in bookmark.attrib['tag'].split(TAG_SEPARATOR):
+                    garbage.append(bookmark)
+
+        for bookmark in garbage:
+            root.remove(bookmark)
+
+    def search_tags_not(self, tags):
+        """
+        Check that no tags are present. Remove the bookmark if they are.
+
+        @param tags: List of tags
+        """
+        root = self.getroot()
+        bookmarks = root.getchildren()
+        garbage = []
+        for tag in tags:
+            for bookmark in bookmarks:
+                if tag in bookmark.attrib['tag'].split(TAG_SEPARATOR):
+                    garbage.append(bookmark)
+
+        for bookmark in garbage:
+            root.remove(bookmark)
+
+    def search_url(self, url):
+        """
+        URL substring match.
+
+        @param url: Substring of an URL
+        """
+        root = self.getroot()
+        bookmarks = root.getchildren()
+        garbage = []
+        for bookmark in bookmarks:
+            if bookmark.attrib['href'].find(url) == -1:
+                garbage.append(bookmark)
+
+        for bookmark in garbage:
+            root.remove(bookmark)
+
+    def search(
+        self,
+        tags = None,
+        ntags = None,
+        desc = None,
+        ndesc = None,
+        note = None,
+        nnote = None,
+        url = None,
+        nurl = None,
+        etags = None,
+        edesc = None,
+        enote = None,
+        eurl = None):
         """
         Find matching bookmarks.
 
-        @param tags: List of tag regexes to match
-        @return: List of URLs
+        @param search: List of tag regexes to match
         """
 
-        tag_patterns = [re.compile(tag) for tag in tags]
+        if tags is not None:
+            self.search_tags_and(tags)
 
-        for node in self.doc.getchildren():
-            for tag_pattern in tag_patterns:
-                match = False
-                for node_tag in node.attrib['tag'].split(' '):
-                    if tag_pattern.match(node_tag):
-                        match = True
-                if not match:
-                    self.doc.remove(node)
+        if ntags is not None:
+            self.search_tags_not(ntags)
 
-        # Update @update
-        #xml_root.setProp('update', datetime.utcnow().isoformat()[:-7] + 'Z')
+        if url is not None:
+            self.search_url(url)
--- filterous_test.py
+++ filterous_test.py
@@ -14,64 +14,160 @@ __url__ = 'http://filterous.sourceforge.net/'
 __copyright__ = 'Copyright (C) 2010 Victor Engmark'
 __license__ = 'GPLv3'
 
+from copy import deepcopy
 import unittest
+import xml.etree.ElementTree as ET
 
 import filterous
 
-class TestValid(unittest.TestCase):
-    """Framework for basic tests."""
+XML = '''<posts tag="" total="3" update="1900-01-01T00:00:00Z" user="none">\n\
+  <post \
+description="Delicious Command Line Filter" \
+extended="" \
+hash="f255818ac26a7e0f19c01005630ea3e1" \
+href="http://filterous.sourceforge.net/" \
+meta="c4ec8ef1570b7c0387b14d7d5f1729cf" \
+tag="Delicious CLI search software opensource" \
+time="2010-02-09T10:30:15Z" \
+/>\n\
+  <post description="Example web page description" \
+extended="" \
+hash="8822003bb3cf9b25b77a2b283944f016" \
+href="https://example.org/path/some.file?example=param#fragment" \
+meta="a4003e2e2bf880e0d316c31d9de686e1" \
+tag="video tosee seen ★★★★★" \
+time="2010-02-08T22:46:39Z" \
+/>\n\
+  <post description="Python Programming Language" \
+extended="" \
+hash="ff4faae39965af2df932214e2648428d" \
+href="http://python.org/" \
+meta="54f0fdb28137f2701ddd01c79713e429" \
+tag="Python programming language opensource software development reference" \
+time="2006-12-01T15:00:52Z" \
+/>\n</posts>'''
+
+def _posts(bookmarks):
+    """Element objects for each post"""
+    return bookmarks.getroot().getchildren()
+
+class TestAll(unittest.TestCase):
+    """Framework for tag AND tests."""
+    # pylint: disable-msg=R0904
 
     def setUp(self):
         """Get XML file contents."""
         # pylint: disable-msg=C0103
-        with open('all.xml') as fp:
-            self.xml = fp.read()
-        self.bookmarks = filterous.Bookmarks(self.xml)
+        self.bookmarks = filterous.DeliciousBookmarks(
+            ET.fromstring(XML))
 
-    def tearDown(self):
-        """"""
-        # pylint: disable-msg=C0103
 
     def test_empty_tag(self):
-        """Empty tag; should not change."""
-        self.bookmarks.search(tags = [''])
+        """Empty tag; should have no posts left."""
+        self.bookmarks.search(tags = [u''])
         self.assertEqual(
-            str(self.bookmarks),
-            str(filterous.Bookmarks(self.xml)))
+            _posts(self.bookmarks),
+            [])
+
+
+    def test_separator_tag(self):
+        """Search for tag separator; should have no posts left."""
+        self.bookmarks.search(tags = [filterous.TAG_SEPARATOR])
+        self.assertEqual(
+            _posts(self.bookmarks),
+            [])
+
 
     def test_simple_tag(self):
-        """Simple tag used in at least one bookmark;
-        should not include all the lines from the original."""
-        self.bookmarks.search(tags = ['tosee'])
-        self.assertNotEqual(
-            str(self.bookmarks),
-            str(filterous.Bookmarks(self.xml)))
+        """Simple tag used in one bookmark; should have 1 post left."""
+        self.bookmarks.search(tags = [u'tosee'])
+        self.assertEqual(
+            len(_posts(self.bookmarks)),
+            1)
+
 
     def test_unused_tag(self):
         """Unused tag; should have no posts left."""
-        self.bookmarks.search(tags = ['klaxbar'])
+        self.bookmarks.search(tags = [u'klaxbar'])
         self.assertEqual(
-            len(str(self.bookmarks).splitlines()),
-            len(str(filterous.Bookmarks(self.xml)).splitlines()) - 3)
+            _posts(self.bookmarks),
+            [])
+
 
     def test_unicode_tag(self):
-        """Unicode tag used in at least one bookmark;
-        should not include all the lines from the original."""
-        self.bookmarks.search(tags = ['★★★★★'])
-        self.assertNotEqual(
-            str(self.bookmarks),
-            str(filterous.Bookmarks(self.xml)))
+        """Unicode tag used in one bookmark; should have 1 post left."""
+        self.bookmarks.search(tags = [u'★★★★★'])
+        self.assertEqual(
+            len(_posts(self.bookmarks)),
+            1)
+
 
     def test_substring_tag(self):
-        """Unused tag which is a substring of a used tag; should not change."""
-        self.bookmarks.search(tags = ['★'])
+        """Unused tag which is a substring of a used tag;
+        should have no posts left."""
+        self.bookmarks.search(tags = [u'★'])
         self.assertEqual(
-            str(self.bookmarks),
-            str(filterous.Bookmarks(self.xml)))
+            _posts(self.bookmarks),
+            [])
+
+    def test_empty_tag_not(self):
+        """Empty tag; should not change."""
+        self.bookmarks.search(ntags = [u''])
+        self.assertEqual(
+            ET.tostring(self.bookmarks.getroot(), 'utf-8'),
+            XML)
+
+
+    def test_separator_tag_not(self):
+        """Search for tag separator; should not change."""
+        self.bookmarks.search(ntags = [filterous.TAG_SEPARATOR])
+        self.assertEqual(
+            ET.tostring(self.bookmarks.getroot(), 'utf-8'),
+            XML)
+
+
+    def test_simple_tag_not(self):
+        """Simple tag used in one bookmark; should have N-1 posts left."""
+        self.bookmarks.search(ntags = [u'tosee'])
+        self.assertEqual(
+            len(ET.tostring(self.bookmarks.getroot(), 'utf-8').splitlines()),
+            len(XML.splitlines()) - 1)
+
+
+    def test_unused_tag_not(self):
+        """Unused tag; should not change."""
+        self.bookmarks.search(ntags = [u'klaxbar'])
+        self.assertEqual(
+            ET.tostring(self.bookmarks.getroot(), 'utf-8'),
+            XML)
+
+
+    def test_unicode_tag_not(self):
+        """Unicode tag used in one bookmark; should have N-1 posts left."""
+        self.bookmarks.search(ntags = [u'★★★★★'])
+        self.assertEqual(
+            len(ET.tostring(self.bookmarks.getroot(), 'utf-8').splitlines()),
+            len(XML.splitlines()) - 1)
+
+
+    def test_substring_tag_not(self):
+        """Unused tag which is a substring of a used tag; should not change."""
+        self.bookmarks.search(ntags = [u'★'])
+        self.assertEqual(
+            ET.tostring(self.bookmarks.getroot(), 'utf-8'),
+            XML)
+
+    def test_simple_url(self):
+        """Domain name match"""
+        self.bookmarks.search(url = u'example.org')
+        self.assertEqual(
+            len(_posts(self.bookmarks)),
+            1)
 
 def main():
     """Command line options checkpoint"""
     unittest.main()
+
 
 if __name__ == '__main__':
     main()
