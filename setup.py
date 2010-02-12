11e2a487f015b6e234ef22e390b5c601d97e2f36
Victor Engmark <victor.engmark@gmail.com>
2010-02-12 16:43:31 +0000

    Setup script.
    git-svn-id: https://filterous.svn.sourceforge.net/svnroot/filterous@11 5632bb97-7f59-4fb5-8ea0-cd9efd6acbbd

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='Filterous',
+    version='0.5',
+    description='Delicious Command Line Filter',
+    author='Victor Engmark',
+    author_email='victor.engmark@gmail.com',
+    url='http://vcard-module.sourceforge.net/',
+    py_modules=['filterous'])
