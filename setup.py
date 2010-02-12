ac1da598b62105446dfe1b8dd891156712d19345
Victor Engmark <victor.engmark@gmail.com>
2010-02-12 17:26:00 +0000

    Build script.
    git-svn-id: https://filterous.svn.sourceforge.net/svnroot/filterous@14 5632bb97-7f59-4fb5-8ea0-cd9efd6acbbd

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='Filterous',
-    version='0.5',
+    version='0.5.1',
     description='Delicious Command Line Filter',
     author='Victor Engmark',
     author_email='victor.engmark@gmail.com',
